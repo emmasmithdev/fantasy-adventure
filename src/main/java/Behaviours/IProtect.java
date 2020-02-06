1a799107022ff4a681ff1c9fecfa54bba55b7a9a
Emma <emmagray183@gmail.com>
2020-02-06 12:42:17 +0000

    knight class, IFight and IProtect interfaces

---
+++ src/main/java/Behaviours/IProtect.java
@@ -0,0 +1,6 @@
+package Behaviours;
+
+public interface IProtect {
+
+    public String protect();
+}
---
+++ src/main/java/Knight.java
@@ -0,0 +1,30 @@
+import Behaviours.IFight;
+import Behaviours.IProtect;
+
+public class Knight extends Player {
+
+    private IFight weapon;
+    private IProtect defence;
+
+    public Knight(String name, IFight weapon, IProtect defence) {
+        super(name);
+        this.weapon = weapon;
+        this.defence = defence;
+    }
+
+    public IFight getWeapon() {
+        return weapon;
+    }
+
+    public IProtect getDefence() {
+        return defence;
+    }
+
+    public void setWeapon(IFight weapon) {
+        this.weapon = weapon;
+    }
+
+    public void setDefence(IProtect defence) {
+        this.defence = defence;
+    }
+}
