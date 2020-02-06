3e54bb8d3046e20f47fe83c41c0b18bf72069ff1
Emma <emmagray183@gmail.com>
2020-02-06 12:31:47 +0000

    player abstract class

---
+++ src/main/java/Player.java
@@ -0,0 +1,18 @@
+public abstract class Player {
+
+    private String name;
+    private int health;
+
+    public Player(String name) {
+        this.name = name;
+        this.health = 10;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+}
